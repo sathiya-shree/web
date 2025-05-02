a661ba6d45623f030ff436ff4233cf2b504afead
sathiya-shree <159093269+sathiya-shree@users.noreply.github.com>
2025-05-02 21:05:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Adding a simple interactivity: change header background color when clicked
+document.querySelector('header').addEventListener('click', () => {
+    document.body.style.backgroundColor = '#e0f7fa';
+});
